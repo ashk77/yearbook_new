38caf8d39a12185957d0f9c1fda3b9147ae95677
MaxAsif <maxasif@gmail.com>
2018-03-16 21:18:43 +0530

    ADS

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Geocoder\Laravel\Providers\GeocoderService::class,
+        
 
         /*
          * Package Service Providers...
